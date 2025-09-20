191a887fa915a0fc2fdba917d0b940062d577ebb
Nikhil Singh <teamsinister43@gmail.com>
2025-09-20 20:14:59 +0530

    Add files via upload

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,13 +2,14 @@ import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import ChatWidget from "./components/ChatRoom/ChatWidget"
+import { Analytics } from "@vercel/analytics/next"
 
 function Layout() {
   return (
     <>
       <Header />
       <ChatWidget /> {/* Floating chat bot icon */}
-      
+      <Analytics />
       <Outlet />
       <Footer />
     </>
