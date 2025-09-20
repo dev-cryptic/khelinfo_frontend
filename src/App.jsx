68cde393acc3830500504cbbb873b12d3dad8f52
Nikhil Singh <teamsinister43@gmail.com>
2025-09-20 20:20:44 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import { database } from "./firebase";
 import "./i18n";
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Analytics />
     </>
   )
 }
